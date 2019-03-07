e76470b557958875a5c2837dd1f3d7b77864afa2
YoungjooHan <urdlove@gmail.com>
2019-03-07 22:01:14 +0900

    'day03'

---
+++ src/main/java/day03/Test04.java
@@ -0,0 +1,15 @@
+package day03;
+
+public class Test04 {
+  public static void main(String[] args) {
+    int[] arr = new int[5];
+//    for (int item : arr) {
+//      System.out.println(item);
+//    }
+
+    for (int i = 0; i < arr.length; i++) {
+//      System.out.println(arr[i]);
+      System.out.println(i);
+    }
+  }
+}
