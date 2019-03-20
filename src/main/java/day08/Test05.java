bcaffecd9260f03b490e4f828585a128b07502aa
YoungjooHan <urdlove@gmail.com>
2019-03-20 20:49:57 +0900

    ch10/Test01

---
+++ src/main/java/day08/Test05.java
@@ -0,0 +1,53 @@
+package day08;
+
+import java.util.Scanner;
+
+public class Test05 {
+  public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);
+
+    System.out.print("계산식?(예: 12 * 2) ");
+    int a = keyboard.nextInt();
+    String op = keyboard.next();
+    int b = keyboard.nextInt();
+
+    keyboard.close();
+
+    int result = 0;
+    switch (op) {
+      case "+":
+        result = plus(a, b);
+        break;
+      case "-":
+        result = minus(a, b);
+        break;
+      case "*":
+        result = multiple(a, b);
+        break;
+      case "/":
+        result = divide(a, b);
+        break;
+      default:
+        System.out.println("지원하지 않는 연산자입니다.");
+        return; // main() 메서드를 그만 실행하고 나간다. 즉 JVM 종료!
+    }
+
+    System.out.printf("%d %s %d = %d\n", a, op, b, result);
+  }
+
+  static int plus(int a, int b) {
+    return a + b;
+  }
+
+  static int minus(int a, int b) {
+    return a - b;
+  }
+
+  static int multiple(int a, int b) {
+    return a * b;
+  }
+
+  static int divide(int a, int b) {
+    return a / b;
+  }
+}
